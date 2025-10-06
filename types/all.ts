f4a6c5e66558085817ca81126d075a222281386e
rahul-p19 <pandeyrahul.work@gmail.com>
2025-10-06 17:59:33 +0530

    fix build issue

--- types/all.ts
+++ types/all.ts
@@ -25,9 +25,9 @@ export type Event = {
   contact: Contact[];
   faq: FAQ[];
   sponsors: string[];
-  day: string;
-  image: string;
-  previewText: string;
+  day?: string;
+  image?: string;
+  previewText?: string;
 };
 
 export enum Role {
